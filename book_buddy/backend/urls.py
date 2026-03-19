44ccc0994bd9ebde0b0cf622e276a5f566a6a52c
Muhammed Mirza PN <mirzzpc1289@gmail.com>
2026-03-19 10:58:58 +0530

    Update handling of page number from recommendations

--- book_buddy/backend/urls.py
+++ book_buddy/backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -25,6 +26,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', lambda request: HttpResponse('ok')),
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
